5b4f64f7317abf404afb4e1ff5ff89a38cb5cf49
Minokinad <danik.ivacewil@bk.ru>
2025-03-21 23:11:08 +0300

    Add post method

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI, Query
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.staticfiles import StaticFiles
 
+from pydantic import BaseModel
+
 app = FastAPI(docs_url=None, redoc_url=None)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -17,6 +19,14 @@ def get_hotels(
         stars: Optional[int] = Query(None, ge=1, le=5),
 ):
     return location, date_from, date_to
+
+class BookingRequest(BaseModel):
+    hotel_id: int
+    date_from: date
+    date_to: date
+@app.post("/bookings/")
+def book(booking: BookingRequest):
+    pass
 @app.get("/docs")
 async def custom_swagger_ui():
     return get_swagger_ui_html(
