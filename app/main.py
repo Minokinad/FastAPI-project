5d915dcfed58f4deb9df2a66f5011a832b513e75
Minokinad <danik.ivacewil@bk.ru>
2025-03-21 22:52:18 +0300

    Add custom documentation & get method

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from datetime import date
+from typing import Optional
 
-app = FastAPI()
+from fastapi import FastAPI, Query
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
 
-@app.get("/")
-def get():
-    return "Hello World!"
+app = FastAPI(docs_url=None, redoc_url=None)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.get("/hotels/")
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+):
+    return location, date_from, date_to
+@app.get("/docs")
+async def custom_swagger_ui():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        swagger_js_url="/static/swagger-ui/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui/swagger-ui.css",
+    )
